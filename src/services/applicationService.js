4a59b485e59369430b77ddf48a57b5477ce4cff7
CaMiMtoto <jeanpaulcami@live.com>
2022-03-21 16:36:25 +0200

    nothing

--- src/services/applicationService.js
+++ src/services/applicationService.js
@@ -4,19 +4,18 @@ const config = require("../config.json");
 
 const apiEndpoint = config.apiUrl + "/application";
 
-
 export function getApplications() {
-    return http.get(apiEndpoint + '/all');
+  return http.get(apiEndpoint + "/all");
 }
 
 export function getApplication(id) {
-    return http.get(apiEndpoint + '/' + id);
+  return http.get(apiEndpoint + "/" + id);
 }
 
 export function submitApplication(application) {
-    return http.post(apiEndpoint + '/submit', application);
+  return http.post(apiEndpoint + "/submit", application);
 }
 
 export function changeApplicationStatus(id, status) {
-    return http.post(`${apiEndpoint}/${id}?status=${status}`);
+  return http.post(`${apiEndpoint}/${id}?status=${status}`);
 }
